c2055ad4c6c6dafb2d57a1c2d43c9d7d833b2096
tunip3 <ts-gaming@outlook.com>
2018-09-24 18:23:53 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace szs_unpacker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            szs.Text = "path\\to\\szs.file";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+
+
+            openFileDialog1.InitialDirectory = @"C:\";
+
+            openFileDialog1.Title = "Browse szs Files";
+
+
+
+            openFileDialog1.CheckFileExists = true;
+
+            openFileDialog1.CheckPathExists = true;
+
+
+
+            openFileDialog1.DefaultExt = "szs";
+
+            openFileDialog1.Filter = "szs files (*.szs)|*.szs|All files (*.*)|*.*";
+
+            openFileDialog1.FilterIndex = 2;
+
+            openFileDialog1.RestoreDirectory = true;
+
+
+
+            openFileDialog1.ReadOnlyChecked = true;
+
+            openFileDialog1.ShowReadOnly = true;
+
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+
+                szs.Text = openFileDialog1.FileName;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = "SARCCompress\\sarc_tool.exe";
+            process.StartInfo.Arguments = szs.Text;
+            // uncomment if you want it maximised process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();
+            string ext = Path.GetExtension(szs.Text);
+            MessageBox.Show("szs file unpacked to "  + (szs.Text.Replace(ext ,"")));
+        }
+    }
+}
